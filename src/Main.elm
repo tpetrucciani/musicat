b8c1161a30b59c68c68ea7dbebe8771d4600ab04
Tommaso Petrucciani <tommaso.petrucciani@gmail.com>
2018-12-26 16:44:25 +0100

    Modified to use JSON data.

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,10 @@
 module Main exposing (Model(..), Msg(..), init, main, subscriptions, update, view)
 
 import Browser
-import Html exposing (Html, pre, text)
+import Html exposing (Html, a, div, img, text)
+import Html.Attributes exposing (href, src)
 import Http
+import Json.Decode exposing (Decoder, field, list, map, map2, map4, string)
 
 
 
@@ -10,7 +12,7 @@ import Http
 
 
 main =
-    Browser.element
+    Browser.document
         { init = init
         , update = update
         , subscriptions = subscriptions
@@ -23,19 +25,76 @@ main =
 
 
 type Model
-    = Failure
+    = Failure String
     | Loading
-    | Success String
+    | Success Catalogue
+
+
+type alias Catalogue =
+    { albums : List Album }
+
+
+type alias Album =
+    { cover : CoverPath
+    , spotify : SpotifyId
+    , entries : List Entry
+    , comment : CommentString
+    }
+
+
+type alias Entry =
+    { genre : Genre
+    , artist : Artist
+    }
+
+
+type alias CoverPath =
+    String
+
+
+type alias SpotifyId =
+    String
+
+
+type alias Genre =
+    String
+
+
+type alias Artist =
+    String
+
+
+type alias CommentString =
+    String
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
     ( Loading
     , Http.get
-        { url = "/_data/albums"
-        , expect = Http.expectString GotText
+        { url = "/_data/catalogue"
+        , expect = Http.expectJson GotCatalogue catalogueDecoder
         }
     )
+
+
+catalogueDecoder : Decoder Catalogue
+catalogueDecoder =
+    map Catalogue (field "albums" (list albumDecoder))
+
+
+albumDecoder : Decoder Album
+albumDecoder =
+    map4 Album
+        (field "cover" string)
+        (field "spotify" string)
+        (field "entries" (list entryDecoder))
+        (field "comment" string)
+
+
+entryDecoder : Decoder Entry
+entryDecoder =
+    map2 Entry (field "genre" string) (field "artist" string)
 
 
 
@@ -43,19 +102,22 @@ init _ =
 
 
 type Msg
-    = GotText (Result Http.Error String)
+    = GotCatalogue (Result Http.Error Catalogue)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        GotText result ->
+        GotCatalogue result ->
             case result of
-                Ok fullText ->
-                    ( Success fullText, Cmd.none )
+                Ok catalogue ->
+                    ( Success catalogue, Cmd.none )
+
+                Err (Http.BadBody err) ->
+                    ( Failure err, Cmd.none )
 
                 Err _ ->
-                    ( Failure, Cmd.none )
+                    ( Failure "Error", Cmd.none )
 
 
 
@@ -71,14 +133,30 @@ subscriptions model =
 -- VIEW
 
 
-view : Model -> Html Msg
+view : Model -> Browser.Document Msg
 view model =
+    { title = "Album catalogue"
+    , body = [ viewBody model ]
+    }
+
+
+viewBody : Model -> Html msg
+viewBody model =
     case model of
-        Failure ->
-            text "I was unable to load your book."
+        Failure err ->
+            text err
 
         Loading ->
             text "Loading..."
 
-        Success fullText ->
-            pre [] [ text fullText ]
+        Success catalogue ->
+            div []
+                (List.map displayAlbum catalogue.albums)
+
+
+displayAlbum : Album -> Html msg
+displayAlbum album =
+    div []
+        [ a [ href ("spotify:album:" ++ album.spotify) ]
+            [ img [ src ("_data/covers/" ++ album.cover) ] [] ]
+        ]
